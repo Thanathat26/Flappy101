40d94026f71ff776b4d21484fb0bb59810bc57dc
Thanathat chitnarong <6610110112@psu.ac.th>
2023-12-26 06:50:32 +0700

    screen

--- Flappy.py
+++ Flappy.py
@@ -1,6 +1,14 @@
 from kivy.app import App
 from kivy.lang import Builder 
 from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class Firstscreen(Screen):
+    pass
+class Seconscreen(Screen):
+    pass
+class Screenmamage(ScreenManager):
+    pass
 Builder.load_file('flappy.kv')
 class MyApp(Widget):
     pass
