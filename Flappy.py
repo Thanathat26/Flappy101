0d70557e4875b8945503c4a81bc2a8d107c27760
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2023-12-26 10:48:32 +0700

    clound + floor

--- Flappy.py
+++ Flappy.py
@@ -1,11 +1,13 @@
 from kivy.app import App
 from kivy.lang import Builder
-
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 Builder.load_file('flappy.kv')
-
+class Background(Widget):
+    pass
 class Gameflappy(App):
-    def build(self):
-        return Builder.load_file('flappy.kv')
+    pass
+
 
 if __name__ == '__main__':
     Gameflappy().run()
