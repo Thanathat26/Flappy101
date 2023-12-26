25c1ce8c405c1437b26446272608827615a3b7d9
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2023-12-26 11:39:04 +0700

    cloud_location

--- Flappy.py
+++ Flappy.py
@@ -2,8 +2,10 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.graphics.texture import Texture
 Builder.load_file('flappy.kv')
 class Background(Widget):
+    texture = ObjectProperty(None)
     pass
 class Gameflappy(App):
     def build(self):
