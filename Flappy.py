86e7f99307bd2d190ccfc3151dd24caf41cf4015
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2023-12-26 07:22:08 +0700

    Debug

--- Flappy.py
+++ Flappy.py
@@ -1,21 +1,11 @@
 from kivy.app import App
-from kivy.lang import Builder 
-from kivy.uix.widget import Widget
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
 
-class Firstscreen(Screen):
-    pass
-class Seconscreen(Screen):
-    pass
-class Screenmamage(ScreenManager):
-    pass
- 
+Builder.load_file('flappy.kv')
+
 class Gameflappy(App):
     def build(self):
-        Builder.load_file('flappy.kv')
-        return ScreenManager()
+        return Builder.load_file('flappy.kv')
 
 if __name__ == '__main__':
     Gameflappy().run()
-    
-    
