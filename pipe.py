be413e091dfd86347489fe560a9ef849bcd8b183
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2023-12-28 22:36:45 +0700

    Fail pipe

--- pipe.py
+++ pipe.py
@@ -1,20 +1,31 @@
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty,ObjectProperty,ListProperty
+from kivy.properties import NumericProperty, ObjectProperty, ListProperty
 from kivy.uix.image import Image
-class Pipe(Widget):
-     Gap_size = NumericProperty(60)
-     cap_size = NumericProperty(20)
-     pipe_center = NumericProperty(0)
-     bottom_body_position = NumericProperty(0)
-     bottom_cap_position = NumericProperty(0)
-     top_body_position = NumericProperty(0)
-     top_cap_position = NumericProperty(0)
-     pipe_body_texture = ObjectProperty
-     lower_pipe = ListProperty((0,0,1,0,1,1,0,1))
-     Top_pipe = ListProperty((0,0,1,0,1,1,0,1))
 
-     def __init__(self, **kwargs):
+class Pipe(Widget):
+    Gap_size = NumericProperty(60)
+    cap_size = NumericProperty(20)
+    pipe_center = NumericProperty(0)
+    bottom_body_position = NumericProperty(0)
+    bottom_cap_position = NumericProperty(0)
+    top_body_position = NumericProperty(0)
+    top_cap_position = NumericProperty(0)
+    pipe_body_texture = ObjectProperty(None)
+    lower_pipe = ListProperty((0, 0, 1, 0, 1, 1, 0, 1))
+    Top_pipe = ListProperty((0, 0, 1, 0, 1, 1, 0, 1))
+
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.pipe_body_texture = Image(source='pipe_body.png').texture
         self.pipe_body_texture.wrap = 'repeat'
-        self.pipe_body_texture.uvsize = (1, -1)
+
+    def on_size(self, *args):
+        lower_body_size = self.bottom_cap_position - self.bottom_body_position
+
+        self.lower_pipe[5] = lower_body_size / 20.
+        self.lower_pipe[7] = lower_body_size / 20.
+
+        top_body_size = self.pipe_center - self.top_body_position
+
+        self.Top_pipe[5] = top_body_size / 20.
+        self.Top_pipe[7] = top_body_size / 20.
