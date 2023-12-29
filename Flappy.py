faa5efadaa30ee7017bfcc2dc6c0c4a2e179db8f
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2023-12-29 11:14:18 +0700

    finaly bird

--- Flappy.py
+++ Flappy.py
@@ -1,54 +1,30 @@
+# main.py
+
 from kivy.app import App
-from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
-from kivy.graphics.texture import Texture
-from kivy.uix.image import Image
-from kivy.animation import Animation
-from kivy.core.window import Window
 from kivy.clock import Clock
-from random import randint
+from kivy.lang import Builder
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
+from kivy.graphics import Rectangle
+from kivy.uix.image import Image
+from kivy.properties import ObjectProperty
 
-from pipe import Pipe
 Builder.load_file('flappy.kv')
-class Background(Widget):
-    cloud_texture = ObjectProperty(None)
-    floor_texture = ObjectProperty(None)
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        self.cloud_texture = Image(source="cloud3.png").texture
-        self.cloud_texture.wrap = 'repeat'
-        self.cloud_texture.uvsize = (Window.width / self.cloud_texture.width, -1)
-
-        self.floor_texture = Image(source="floor3.png").texture
-        self.floor_texture.wrap = 'repeat'
-        self.floor_texture.uvsize = (Window.width / self.cloud_texture.width, -1)
-
-
-    def scroll_texture(self, time_passed):
-        self.cloud_texture.uvpos = ((self.cloud_texture.uvpos[0] + time_passed) % Window.width, self.cloud_texture.uvpos[1])
-        self.floor_texture.uvpos = ((self.floor_texture.uvpos[0] + time_passed) % Window.width, self.floor_texture.uvpos[1])
-        texture = self.property('cloud_texture')
-        texture.dispatch(self)
-        texture = self.property('floor_texture')
-        texture.dispatch(self)
 
 class GameWidget(Widget):
+    bird_pos = ObjectProperty((100, 200))
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self._keyboard = Window.request_keyboard(
-            self._on_keyboard_closed, self
-        )
+            self._on_keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_key_down)
         self._keyboard.bind(on_key_up=self._on_key_up)
         self.pressed_keys = set()
         Clock.schedule_interval(self.move_step, 0)
         with self.canvas:
-            self.hero = Rectangle(
-                source='.png', pos=(0, 0), size=(100, 100)
-            )
+            self.bird = Rectangle(source='bird2.png', pos=self.bird_pos, size=(100, 100))
 
     def _on_keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_key_down)
@@ -58,23 +34,17 @@ class GameWidget(Widget):
     def _on_key_down(self, keyboard, keycode, text, modifiers):
         print('down', text)
         self.pressed_keys.add(text)
-        
-        # เมื่อกดปุ่ม 'w' ให้กระโดด
-        if text == 'w':
-            self.jump()
 
     def _on_key_up(self, keyboard, keycode):
         text = keycode[1]
         print('up', text)
-
         if text in self.pressed_keys:
             self.pressed_keys.remove(text)
 
     def move_step(self, dt):
-        cur_x = self.hero.pos[0]
-        cur_y = self.hero.pos[1]
+        cur_x = self.bird.pos[0]
+        cur_y = self.bird.pos[1]
         step = 100 * dt
-
         if 'w' in self.pressed_keys:
             cur_y += step
         if 's' in self.pressed_keys:
@@ -83,21 +53,44 @@ class GameWidget(Widget):
             cur_x -= step
         if 'd' in self.pressed_keys:
             cur_x += step
+        self.bird.pos = (cur_x, cur_y)
 
-        self.hero.pos = (cur_x, cur_y)
+class Background(Widget):
+    cloud_texture = ObjectProperty(None)
+    floor_texture = ObjectProperty(None)
+    bird_texture = ObjectProperty(None)
 
-    def jump(self):
-        # เพิ่มโค้ดที่นี่เพื่อให้ตัวละครกระโดด
-        pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cloud_texture = Image(source="cloud3.png").texture
+        self.cloud_texture.wrap = 'repeat'
+        self.cloud_texture.uvsize = (Window.width / self.cloud_texture.width, -1)
+
+        self.floor_texture = Image(source="floor3.png").texture
+        self.floor_texture.wrap = 'repeat'
+        self.floor_texture.uvsize = (Window.width / self.cloud_texture.width, -1)
+
+        self.bird_texture = Image(source='bird2.png').texture
+
+    def scroll_texture(self, time_passed):
+        self.cloud_texture.uvpos = ((self.cloud_texture.uvpos[0] + time_passed) % Window.width, self.cloud_texture.uvpos[1])
+        self.floor_texture.uvpos = ((self.floor_texture.uvpos[0] + time_passed) % Window.width, self.floor_texture.uvpos[1])
+        self.bird_texture.uvpos = ((self.bird_texture.uvpos[0] + time_passed) % Window.width, self.bird_texture.uvpos[1])
+        texture = self.property('cloud_texture')
+        texture.dispatch(self)
+        texture = self.property('floor_texture')
+        texture.dispatch(self)
+        texture = self.property('bird_texture')
+        texture.dispatch(self)
 
 class Gameflappy(App):
     def build(self):
         layout = FloatLayout()
-        self.bird = Bird()
         background = Background()
+        game_widget = GameWidget()
         layout.add_widget(background)
-        layout.add_widget(self.bird)
         Clock.schedule_interval(background.scroll_texture, 1/60.)
+        layout.add_widget(game_widget)
         return layout
 
 if __name__ == '__main__':
