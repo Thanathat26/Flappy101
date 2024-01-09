dbee79c710408a24588b2158d4f857ae9838ffc8
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2024-01-09 12:43:26 +0700

    interface flappy

--- Main.py
+++ Main.py
@@ -1,16 +1,13 @@
+#mainขั้นกว่า
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
-from kivy.clock import Clock
-from kivy.lang import Builder
-from kivy.uix.widget import Widget
-from kivy.core.window import Window
-from kivy.graphics import Rectangle
-from kivy.uix.image import Image
-from kivy.properties import ObjectProperty
-from PIL import Image as PilImage
-from io import BytesIO
-from kivy.core.audio import SoundLoader
-from random import randint
-from kivy.uix.popup import Popup
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from kivy.uix.label import Label
-Builder.load_file('flapp.py')
+from kivy.lang import Builder
+Builder.load_file('Flappy.kv')
+
+
+
+
+if __name__ == '__main__':
+    FlappyApp().run()
