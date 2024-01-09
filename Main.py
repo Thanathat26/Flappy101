ff9626df2aa1795f5ee1981fed222525ceaea9c5
Thanathat26 <154845738+Thanathat26@users.noreply.github.com>
2024-01-09 13:05:31 +0700

    game wid

--- Main.py
+++ Main.py
@@ -4,10 +4,13 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.lang import Builder
+from kivy.uix.widget import Widget
 Builder.load_file('Flappy.kv')
-
-
-
-
+Builder.load_file('gameinterface.kv')
+class Mylayout(Widget):
+    pass
+class Button1(App):
+    def build(self):
+        return Mylayout()
 if __name__ == '__main__':
-    FlappyApp().run()
+    Button1().run()
